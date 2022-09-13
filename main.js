dd2a8f29a6694fb41c897f4628b571889e80acba
Kylie-Lang1 <kylielang@pursuit.org>
2022-09-13 13:56:03 -0400

    Display question 1

--- main.js
+++ main.js
@@ -31,6 +31,8 @@ const questionNum = document.querySelector(`.count`)
 const score = document.querySelector(`.score`)
 const main = document.querySelector(`main`)
 const q = document.querySelector(`.q`)
+const submit = document.querySelector(`#submit`)
+const answer = document.querySelector(`.answer`)
 
 //Declaring variables that can be re-assigned vales
 let playerName = ``
@@ -62,6 +64,7 @@ select.addEventListener(`submit`, (event) => {
     category = `${event.target.category.value}`
     difficulty = `${event.target.difficulty.value}`
 
+    //Guard clause with error alert if category and difficulty not selected
     if(category.includes(`Select`) === true || difficulty.includes(`Select`) === true){
         window.alert(`Please select both a category and difficulty level.`)
     } else {
@@ -71,14 +74,16 @@ select.addEventListener(`submit`, (event) => {
         score.classList.remove(`hidden`)
     }
 
+    //Re-assigning selected difficulty to API search parameter value
     if(difficulty === `easy`){
-        difficulty = 100
+        difficulty = easyId
     } else if (difficulty === `medium`){
-        difficulty = 300
+        difficulty = mediumId
     } else if (difficulty === `hard`){
-        difficulty = 600
+        difficulty = hardId
     }
 
+    //Re-assigning selected category to API search parameter value
     if(category.includes(`cities`)){
         category = citiesId
     } else if (category === `Trivia`){
@@ -89,20 +94,39 @@ select.addEventListener(`submit`, (event) => {
         category = tvTrivId
     }
 
+    //Fetching API data for selected search parameters
     fetch(`${BASE_URL}clues?category=${category}&value=${difficulty}`)
     .then(res => res.json())
     .then(res => {
-        questionNum.textContent = `Question ${qCount}:`
         
+        //Random number generator
         let randomNum = []
         for(let i=0; i<15; i++){
-            randomNum[i] = (Math.random()*100).toFixed(0)
+            num = (Math.random()*res.length).toFixed(0)
+            if (!randomNum.includes(num)){
+                randomNum[i] = Number(num) 
+            } else {
+                randomNum[i] = Number(num+1)
+            }
             console.log(randomNum)
         }
 
-        for(let i=0; i<15; i++){
+        //Adding text content for first question
+        questionNum.textContent = `Question ${qCount}:`
+        console.log(res)
+        q.textContent = `${res[randomNum[0]][`question`]}`
 
-        }
+        //Adding event listener for each question submit
+        submit.addEventListener(`click`, (event) => {
+            event.preventDefault()
+
+            answer.innerHTML = `<span>Answer:</span> ${res[randomNum[0]][`answer`]}`
+            
+        })
+
+        // for(let i=0; i<15; i++){
+        //     q.textContent = `${res[randomNum[i]]}`
+        // }
         // console.log(res)
         // q.textContent = `${res[0][`question`]}`
     })
